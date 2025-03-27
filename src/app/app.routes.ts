4e6de6a5e73ac2feb8da79f8e9e202f32293167a
AlshimaaSha <alshimaashahat21@gmail.com>
2025-03-27 06:26:32 +0200

    git product in details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { adminRoutes } from './Main/admin/admin.routing.module';
-import { Routes } from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
 import { CartComponent } from '../app/Cart.main/cart/cart.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { customerRoutes } from './Main/customer/customer.routes';
-import { CardComponent } from './Main/customer/card/card.component';
-import { ProductDetailsComponent } from './Main/customer/product-details/product-details.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 
+
+  
 export const routes: Routes = [
     {
         path: 'cart',
